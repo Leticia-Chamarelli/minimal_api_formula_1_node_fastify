12740ee272f5cae5e91475159bb4662f010395f5
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-03-28 17:53:14 -0300

    Initialize server listening on port 3333

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,7 @@ server.get("/teams", async(request, response)=>{
     response.type("application/json").code(200)
     return [{id: 1, name: "Ferrari" }];
 });
+
+server.listen({port: 3333}, () => {
+    console.log("Server init");
+});

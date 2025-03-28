332b6b5b43b08166f5fe4ff948eaac1bc7e71a38
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-03-28 18:07:13 -0300

    create GET method to list pilots

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,11 @@ server.get("/teams", async(request, response)=>{
     return [{id: 1, name: "Ferrari" }];
 });
 
+server.get("/drivers", async(request, response)=>{
+    response.type("application/json").code(200)
+    return [{id: 1, name: "Max Verstappen", team: "Red Bull Racing"}];
+});
+
 server.listen({port: 3333}, () => {
     console.log("Server init");
 });

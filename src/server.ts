052b0048f180bcf0890ff4a378c7688cc008de87
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-03-31 13:34:19 -0300

    filter drive by id

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import { request } from "http";
 
 const server = fastify({logger: true});
 
@@ -42,6 +43,23 @@ server.get("/teams", async(request, response)=>{
 server.get("/drivers", async(request, response)=>{
     response.type("application/json").code(200)
     return {drivers};
+});
+
+interface DriverParams{
+    id: string
+}
+
+server.get<{Params: DriverParams}>("/drivers/:id", async(request, response) => {
+    const id = parseInt(request.params.id);
+    const driver = drivers.find( d => d.id === id);
+
+    if(!driver){
+        response.type("application/json").code(404);
+        return {message: "Driver not found"}
+    }else{
+        response.type("application/json").code(200);
+        return {driver};
+    }
 });
 
 server.listen({port: 3333}, () => {
